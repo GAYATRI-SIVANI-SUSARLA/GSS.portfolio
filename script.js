af89f387d60fd1deae0e58d0b953b260305a4d47
Gayatri Sivani Susarla <gayatrisivani3010@gmail.com>
2025-03-28 20:14:16 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Smooth Scrolling for Navigation Links
+document.querySelectorAll('.nav-links a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        window.scrollTo({
+            top: targetElement.offsetTop - 60,
+            behavior: 'smooth'
+        });
+    });
+});
